a61cf3468f7db0263d6b07eb6b476bc612294e2c
killeraliens <killeraliens@outlook.com>
2019-12-03 21:37:08 -0700

    skips

--- src/EditBookmark/EditBookmark.test.js
+++ src/EditBookmark/EditBookmark.test.js
@@ -3,42 +3,42 @@ import ReactDOM from 'react-dom';
 //import { BrowserRouter } from 'react-router-dom';
 import { MemoryRouter } from 'react-router';
 import EditBookmark from './EditBookmark';
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import App from '../App'
 import NotFound from '../NotFound/NotFound'
-//import BookmarksContext from '../BookmarksContext';
-//import context from '../testHelpers'
-//const BookmarksContext = jest.genMockFromModule('provider')
-//jest.mock('../__mocks__/provider')
 
-describe.skip('EditBookmark component', () => {
 
-  it('renders when passed good bookmark id', () => {
-    const bookmarkId = 1
-    const wrapper = mount(
-      <MemoryRouter initialEntries={[`/edit-bookmark/${bookmarkId}`]}>
-          <App>
-            {/* <BookmarksContext.Provider context={context} /> */}
-          </App>
 
+describe('EditBookmark component', () => {
+
+  it.skip('renders without crashing', () => {
+    mount(
+      <MemoryRouter initialEntries={['/edit-bookmark/1']}>
+        <EditBookmark />
       </MemoryRouter>
     )
-    expect(wrapper.find(EditBookmark)).toHaveLength(1)
-    expect(wrapper.find(NotFound)).toHaveLength(0)
   })
+  // it.skip('renders when passed good bookmark id', () => {
+  //   const bookmarkId = 1
+  //   const wrapper = mount(
+  //     <MemoryRouter initialEntries={[`/edit-bookmark/${bookmarkId}`]}>
+  //       <App/>
+  //     </MemoryRouter>
+  //   )
+  //   expect(wrapper.find(EditBookmark)).toHaveLength(1)
+  //   expect(wrapper.find(NotFound)).toHaveLength(0)
+  // })
 
-  it('renders not found page when passed bad bookmark id', () => {
-    const bookmarkId = 666
-    const wrapper = mount(
-      <MemoryRouter initialEntries={[`/edit-bookmark/${bookmarkId}`]}>
-        <App>
-          {/* <BookmarksContext.Provider context={context} /> */}
-        </App>
-      </MemoryRouter>
-    )
-    expect(wrapper.find(EditBookmark)).toHaveLength(0)
-    expect(wrapper.find(NotFound)).toHaveLength(1)
-  })
+  // it.skip('renders not found page when passed bad bookmark id', () => {
+  //   const badBookmarkId = 666
+  //   const wrapper = mount(
+  //     <MemoryRouter initialEntries={[`/edit-bookmark/${badBookmarkId}`]}>
+  //       <App/>
+  //     </MemoryRouter>
+  //   )
+  //   expect(wrapper.find(EditBookmark)).toHaveLength(0)
+  //   expect(wrapper.find(NotFound)).toHaveLength(1)
+  // })
 })
 
 
