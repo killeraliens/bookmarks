b71826f0f00bbd6d583fedab01a26095a3c4f6d8
killeraliens <killeraliens@outlook.com>
2019-12-28 20:10:16 -0700

    key named correctly

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export default {
-  // API_ENDPOINT: process.env.NODE_ENV === 'production'
-  //   ? `https://ancient-sands-43748.herokuapp.com/api/bookmarks`
-  //   : 'http://localhost:8000/api/bookmarks',
-  API_ENDPOINT: `https://ancient-sands-43748.herokuapp.com/api/bookmarks`,
-  API_KEY: process.env.REACT_APP_API_KEY
+  API_ENDPOINT: process.env.NODE_ENV === 'production'
+    ? `https://ancient-sands-43748.herokuapp.com/api/bookmarks`
+    : 'http://localhost:8000/api/bookmarks',
+  API_KEY: process.env.NODE_ENV === 'production'
+    ?  process.env.REACT_APP_API_KEY
+    : process.env.API_TOKEN
 }
